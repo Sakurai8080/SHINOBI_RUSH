444bc2b357ba0d80f663eb1efe9afe3559e08696
Sakurai8080 <sakuraitakuya8080@gmail.com>
2023-11-21 00:11:12 +0900

    Itemのベースクラスを作成

---
+++ Assets/Scripts/Items/ItemBase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public abstract class ItemBase : MonoBehaviour, IPoolable
+{
+    #region property
+    public IObservable<Unit> InactiveObserver => _inactiveSubject;
+    #endregion
+
+    #region serialize
+    [SerializeField]
+    private ItemData _itemData = default;
+    #endregion
+
+    #region private
+    private Vector3 _itemPosition;
+    private Rigidbody _rd;
+    private Transform _playerTrans;
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    private Subject<Unit> _inactiveSubject = new Subject<Unit>();
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+        _playerTrans = GameObject.FindGameObjectWithTag(GameTag.Player).transform;
+        _rd = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        _itemPosition = transform.position;
+    }
+
+    private void OnDisable()
+    {
+        _inactiveSubject.OnNext(Unit.Default);
+    }
+
+    #endregion
+
+    #region public method
+    public void ReturnPool()
+    {
+        gameObject.SetActive(false);
+    }
+    #endregion
+
+
+    #region abstract method
+    /// <summary>
+    /// アイテム使用
+    /// </summary>
+    /// <param name="player"></param>
+    public abstract void Use(PlayerController player);
+
+    /// <summary>
+    /// アイテムを戻す処理
+    /// </summary>
+    public abstract void Return();
+    #endregion
+
+    #region private method
+    #endregion
+}
---
+++ Assets/Scripts/Items/ItemData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/ItemData")]
+public class ItemData : ScriptableObject
+{
+    #region property
+    public ItemType ItemType => _itemType;
+    public uint ReserveAmount => _reserveAmount;
+    #endregion
+
+    #region serialize
+    [SerializeField]
+    private ItemType _itemType = default;
+
+    [SerializeField]
+    private uint _reserveAmount = 5;
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+}
+
+public enum ItemType
+{
+    None,
+    EXP,
+    Heal,
+    PowerUp
+}
