efa7c37e7a3b4eeec855b08524602f709097be3a
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-01-18 15:26:49 +0900

    風遁の術を飛ばす仕様から変更

--- Assets/Scripts/Skills/WindSkill.cs
+++ Assets/Scripts/Skills/WindSkill.cs
@@ -24,23 +24,14 @@ public class WindSkill : SkillBase
     [Tooltip("プレイヤーのポジション")]
     [SerializeField]
     private Transform _playerTransform = default;
-
-    [Tooltip("アクティブ時の親オブジェクト")]
-    [SerializeField]
-    private Transform _activedParent = default;
     #endregion
 
     #region private
-    private List<Transform> _enemies = new List<Transform>();
-    private Vector3 _spawnPosition;
-    private float _waitTime = 5.0f;
+    private Transform _spawnPosition;
     private float _attackCoefficient = 5.0f;
-    private Wind currentWind = default;
-    private WindGenerator _windGenerator;
-    private Vector3 _spwnUpPositionOffset = new Vector3(-0.2f, 0.3f, 0.1f);
-    private Vector3 _spwnDownPositionOffset = new Vector3(0.2f, -0.3f, 0.1f);
-    private Vector3 _initialPlayerPos;
-    private bool _isPlayerDown = false;
+    private Wind _currentWind = default;
+    private Vector3 _spawnPositionOffset = new Vector3(0f, 0f, 1f);
+    private float _sizechangeCoefficient = 1.5f;
     #endregion
 
     #region Constant
@@ -54,26 +45,6 @@ public class WindSkill : SkillBase
     {
         base.Awake();
     }
-
-    private void Start()
-    {
-        _initialPlayerPos = _playerTransform.position;
-        _spawnPosition = _playerTransform.position + _spwnUpPositionOffset;
-        _windGenerator = GetComponent<WindGenerator>();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag(GameTag.Enemy))
-            _enemies.Add(other.GetComponent<Transform>());
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        Debug.Log(_enemies.Count());
-        if (other.CompareTag(GameTag.Enemy))
-            _enemies.Remove(other.GetComponent<Transform>());
-    }
     #endregion
 
     #region public method
@@ -82,8 +53,7 @@ public class WindSkill : SkillBase
         Debug.Log($"{SkillType}スキル発動");
         _isSkillActive = true;
         transform.SetParent(_playerTransform);
-        _currentCoroutine = StartCoroutine(SkillActionCroutine());
-        currentWind = _wind;
+        CreateWind(_wind);
     }
 
     public override void SkillUp()
@@ -95,8 +65,11 @@ public class WindSkill : SkillBase
         }
         Debug.Log($"{SkillType}は{_currentSkillLevel}");
         _currentSkillLevel++;
-        currentWind = (_currentSkillLevel >= 5) ? _maxWind : _wind; 
         AttackUpAmount(_attackCoefficient);
+        if (_currentSkillLevel == 4)
+            CreateWind(_maxWind);
+        else
+            _currentWind.SizeUp(_sizechangeCoefficient);
     }
 
     public override void AttackUpAmount(float coefficient)
@@ -106,46 +79,17 @@ public class WindSkill : SkillBase
     #endregion
 
     #region private method
-    private Vector3 SetTarget(Vector3 targetDir)
+    private void CreateWind(Wind wind)
     {
-        Transform nearestEnemy = _enemies.First();
-        float distance = float.MaxValue;
-
-        foreach (Transform enemyTransform in _enemies)
-        {
-            float currentDistance = Vector3.Distance(transform.position, enemyTransform.position);
-            if (currentDistance < distance)
-            {
-                nearestEnemy = enemyTransform;
-                distance = currentDistance;
-            }
-        }
-        return targetDir = (nearestEnemy.position - transform.position);
+        _currentWind?.gameObject?.SetActive(false);
+        _currentWind = Instantiate(wind,transform);
     }
     #endregion
 
     #region coroutine method
     protected override IEnumerator SkillActionCroutine()
     {
-        while (_isSkillActive)
-        {
-            Vector3 targetDir = Vector3.zero;
-            Wind windObj = _windGenerator.WindPool.Rent();
-
-            _isPlayerDown = _initialPlayerPos != _playerTransform.position ? true : false;
-            Vector3 currentSpawnPos = (_isPlayerDown) ? _playerTransform.position + _spwnDownPositionOffset : _spawnPosition;
-            if (windObj != null)
-            {
-                windObj.transform.position = currentSpawnPos;
-                yield return new WaitForSeconds(_waitTime);
-                windObj.gameObject.SetActive(true);
-                yield return new WaitUntil(() => _enemies?.Count() >= 1);
-                Vector3 currentTransform = SetTarget(targetDir);
-                windObj.SetAttackAmount(_currentAttackAmount);
-                windObj.SetVelocity(currentTransform);
-                windObj.transform.SetParent(_activedParent);
-            }
-        }
+        yield return null;
     }
     #endregion
 }
