23a34d2882e2d68c7bf15e4d736df95bafef7d5e
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-01-22 16:33:51 +0900

    AudioManagerの追加とBGM,SEクラスの追加

---
+++ Assets/Scripts/Audio/BGM.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class BGM
+{
+    public string BGMName;
+    public BGMType BGMType;
+    public AudioClip Clip;
+    [Range(0f,1f)]
+    public float Volume = 1f;
+}
+
+public enum BGMType
+{
+    Title,
+    InGame,
+    InGame2,
+    Result
+}
---
+++ Assets/Scripts/Audio/SE.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class SE
+{
+    public string SEName;
+    public SEType SEType;
+    public AudioClip Clip;
+    [Range(0f, 1f)]
+    public float Volume = 1f;
+}
+
+public enum SEType
+{
+    Test
+}
