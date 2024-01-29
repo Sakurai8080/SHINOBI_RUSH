e3437a12738f0f0974768de261de53b90c763fc8
Sakurai8080 <sakuraitakuya8080@gmail.com>
2024-01-29 14:42:11 +0900

    プレイヤーの位置をブールで管理する機能追加

--- Assets/Scripts/Players/PlayerMove.cs
+++ Assets/Scripts/Players/PlayerMove.cs
@@ -12,8 +12,11 @@ using UniRx.Triggers;
 public class PlayerMove : MonoBehaviour
 {
     #region property
+    public IReactiveProperty<bool> OnDown => _onDown;
+    private ReactiveProperty<bool> _onDown = new ReactiveProperty<bool>();
     public IReactiveProperty<bool> OnAvaterd => _onAvatered;
     private ReactiveProperty<bool> _onAvatered = new ReactiveProperty<bool>();
+
     #endregion
 
     #region serialize
@@ -32,7 +35,6 @@ public class PlayerMove : MonoBehaviour
     private Vector3 _firstRotate = default;
     private Vector3 _secondPosition = new Vector3(0,-0.05f,0);
     private Vector3 _secondRotate = new Vector3(0, 0, 180);
-    private Vector3 _avaterEffectRotation = new Vector3(0, 0, 270);
     private BoxCollider _col = default;
     #endregion
 
@@ -54,11 +56,13 @@ public class PlayerMove : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            StartCoroutine(OnAvaterCroutine(_secondPosition,_secondRotate));
+            StartCoroutine(OnAvaterCoroutine(_secondPosition,_secondRotate));
+            _onDown.Value = true;
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            StartCoroutine(OnAvaterCroutine(_firstPosition, _firstRotate));
+            StartCoroutine(OnAvaterCoroutine(_firstPosition, _firstRotate));
+            _onDown.Value = false;
         }
     }
     #endregion
@@ -74,7 +78,7 @@ public class PlayerMove : MonoBehaviour
     #endregion
 
     #region coroutine method
-    private IEnumerator OnAvaterCroutine(Vector3 playerPos, Vector3 playerAngles)
+    private IEnumerator OnAvaterCoroutine(Vector3 playerPos, Vector3 playerAngles)
     {
         if (!_onAvatered.Value) {
             _onAvatered.Value = true;
